c772f2845846cc3923dd3e9a75759acb5c83a037
shahedkh <alkhalailah.shahed@gmail.com>
2022-06-19 00:48:42 +0300

    add meetup details

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
+      title="first"
+      address="street 9"
+      description="aaaaa"
+    />
+  );
+}
+export default MeetupDetails;
