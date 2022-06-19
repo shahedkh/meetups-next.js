f0abd2ed4b6bc2090c409ff72eb848c5b49934b2
shahedkh <alkhalailah.shahed@gmail.com>
2022-06-19 04:39:42 +0300

    mongoDb

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,8 +1,21 @@
-function handler (req , res){
-    if(req.method === 'POST'){
-        const data = req.body
+import { MongoClient } from "mongodb";
 
-        const {title, image , address , description} = data
-    }
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://shahed:Zxy2r1705VG6iUMv@cluster0.y5bxtzl.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db()
+
+    const meetupsCollection = db.collection('meetups')
+
+    const result = await meetupsCollection.insertOne(data)
+
+    client.close()
+
+    res.status(201).json({message : 'Meetup inserted!'})
+  }
 }
 export default handler;
